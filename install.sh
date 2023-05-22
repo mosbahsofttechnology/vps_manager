16bf3bf91926d8483fc364e37ffd269a847b20bd
Abbas Nazari <49541849+abbasnazari-0@users.noreply.github.com>
2023-05-22 19:00:49 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,28 @@
-apt-get install python3 python3-pip unzip nginx gunicorn -y && pkill gunicorn && rm -rf servermanager/ vps_manager/ v11.zip nohup.out v10.zip v12.zip ; git clone https://github.com/abbasnazari-0/vps_manager.git && mv vps_manager/servermanager /etc/nginx/sites-enabled/servermanager && unlink /etc/nginx/sites-enabled/default && nginx -s reload ; pip3 install flask jdatetime jsonpickle psutil ; nohup gunicorn -w 3 vps_manager:app --bind 0.0.0.0:4000 &
+apt-get install python3 python3-pip unzip nginx gunicorn -y 
+
+pkill gunicorn 
+
+rm -rf servermanager/ vps_manager/ v11.zip nohup.out v10.zip v12.zip 
+
+git clone https://github.com/abbasnazari-0/vps_manager.git 
+mv vps_manager/servermanager /etc/nginx/sites-enabled/servermanager 
+unlink /etc/nginx/sites-enabled/default && nginx -s reload 
+
+pip3 install flask jdatetime jsonpickle psutil 
+
+echo "
+[Unit]
+Description=My App Service
+
+[Service]
+ExecStart=gunicorn -w 3 vps_manager:app --bind 0.0.0.0:4000
+WorkingDirectory=/root/vps_manager/
+Restart=always
+
+[Install]
+WantedBy=multi-user.target" >> /etc/systemd/system/manager_vps.service
+
+sudo systemctl daemon-reload
+sudo systemctl start myapp.service
+sudo systemctl enable myapp.service
+
